6fa72be647854d0a75e4a703632baeeb0042a034
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-10 16:57:45 -0400

    Fixed line break warning

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ if ENV == 'dev':
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'postgres://onijnjombokzhh:b71661552e670fb920ae4126dc7f798a3925675cb8fed5dd73a06df9e06d56d1@ec2-34-202-88-122.compute-1.amazonaws.com:5432/d5vubft84tih4p'
+        'postgres://onijnjombokzhh:b71661552e670fb920ae4126dc7f798a3925675cb8'\
+        'fed5dd73a06df9e06d56d1@ec2-34-202-88-122.compute-1.amazonaws.com:543'\
+        '2/d5vubft84tih4p'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # creating database object
