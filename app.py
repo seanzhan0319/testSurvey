6383c8bd61ccd7545f799f04ae70117419acdfba
QuantumScrewdriver <bsk945@gmail.com>
2020-06-11 15:36:01 -0700

    Deploy taking files from qualtrics v2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 from pytrics_get import pytrics_get
 
