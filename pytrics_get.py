628699d787ed7707a543c110253a1e8dcabc4ac6
QuantumScrewdriver <bsk945@gmail.com>
2020-06-15 16:32:39 -0700

    Prior to shift

--- pytrics_get.py
+++ pytrics_get.py
@@ -1,4 +1,9 @@
+import os
 from pytrics.tools import Tools
+from pytrics_rework.pytrics_rework.operations.download import get_survey_json
+from pytrics.common.constants import (
+    ENV_VAR_ABSOLUTE_PATH_TO_DATA_DIR,
+)
 
 def pytrics_get():
 
@@ -8,10 +13,22 @@ def pytrics_get():
         tools.retrieve_survey_response_data('SV_eQl4Bl9zA0b9rHD')
     except EnvironmentError as e:
         print("EnvironmentError")
-        print(e)
         print("Error Encountered")
+        return("Problem with accessing data")
     except Exception as e:
         print("Unknown error")
         print(e)
     finally:
         return("Complete")
+
+def pytrics_data():
+    response_file_json = None
+    abs_path_to_data_dir = os.environ.get(ENV_VAR_ABSOLUTE_PATH_TO_DATA_DIR, '')
+    
+    try:
+        response_file_json = get_survey_json('SV_eQl4Bl9zA0b9rHD', abs_path_to_data_dir)
+    except Exception as e:
+        print("Unknown Exception")
+        print(e)
+    finally:
+        return response_file_json
